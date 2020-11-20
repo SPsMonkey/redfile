5c6040a2e2152402a8746a3fb66c8b199140efad
ldx <450544282@qq.com>
2020-11-20 17:11:01 +0800

    11.20

--- main.py
+++ main.py
@@ -26,9 +26,36 @@ def set_win_center(root, curWidth='', curHight=''):
     # 设置窗口初始大小和位置
     size_xy = '%dx%d+%d+%d' % (curWidth, curHight, cen_x, cen_y)
     root.geometry(size_xy)
+def hello():
+    pass
+def addMenu(root):
+    menubar = tk.Menu(root)
+
+    # create a pulldown menu, and add it to the menu bar
+    filemenu = tk.Menu(menubar, tearoff=0)
+    filemenu.add_command(label="Open", command=hello)
+    filemenu.add_command(label="Save", command=hello)
+    filemenu.add_separator()
+    filemenu.add_command(label="Exit", command=root.quit)
+    menubar.add_cascade(label="File", menu=filemenu)
+
+    # create more pulldown menus
+    editmenu = tk.Menu(menubar, tearoff=0)
+    editmenu.add_command(label="Cut", command=hello)
+    editmenu.add_command(label="Copy", command=hello)
+    editmenu.add_command(label="Paste", command=hello)
+    menubar.add_cascade(label="Edit", menu=editmenu)
+
+    helpmenu = tk.Menu(menubar, tearoff=0)
+    helpmenu.add_command(label="About", command=hello)
+    menubar.add_cascade(label="Help", menu=helpmenu)
+
+    # display the menu
+    root.config(menu=menubar)
 
 if __name__ == '__main__':
     root = tk.Tk()
+    addMenu(root)
     tabview=ttk.Notebook(root)
 
 
